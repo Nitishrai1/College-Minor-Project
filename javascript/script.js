7bd03a7884d2140fbf8936c712af1509cf382da7
Nitishrai1 <nitish.12208500@lpu.in>
2024-04-13 23:32:48 +0530

    index-page-update

--- javascript/script.js
+++ javascript/script.js
@@ -18,3 +18,52 @@ var swiper = new Swiper('.swiper-container', {
       prevEl: '.swiper-button-prev',
     }
   });
+
+  var div=document.querySelector(".Residential-property");
+  var display=0;
+  function hideandShow(){
+    if(display==1){
+      div.style.display='block';
+      display=0;
+    }
+    else{
+      div.style.display='none';
+      display=1;
+    }
+  }
+  var div2=document.querySelector(".commericail-section");
+  var display=0;
+  function hideandShow2(){
+    if(display==1){
+      div2.style.display='block';
+      display=0;
+    }
+    else{
+      div2.style.display='none';
+      display=1;
+    }
+  }
+
+  var div3=document.querySelector(".industrial-section");
+  function hideandShow3(){
+    if(display==1){
+      div3.style.display='block';
+      display=0;
+    }
+    else{
+      div3.style.display='none';
+      display=1;
+    }
+  }
+
+  var div4=document.querySelector(".agricultural-section");
+  function hideandShow4(){
+    if(display==1){
+      div4.style.display='block';
+      display=0;
+    }
+    else{
+      div4.style.display='none';
+      display=1;
+    }
+  }
